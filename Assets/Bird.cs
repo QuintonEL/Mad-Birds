878b9b58b34de02ad1fa42cf6e60caec9ef36dda
QuintonEL <quintonlaborde@gmail.com>
2020-05-01 17:02:38 -0600

    added blocks in and more bird physics

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 public class Bird : MonoBehaviour
 {
 	private Vector3 _initialPosition;
+	private bool _birdWasLaunched;
+	private float _timeSittingAround;
+
 	[SerializeField] private float _launchPower = 250;
 
 	private void Awake() {
@@ -11,14 +14,26 @@ public class Bird : MonoBehaviour
 	}
 
 	private void Update() {
-		if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -10) {
+		GetComponent<LineRenderer>().SetPosition(0, _initialPosition);
+		GetComponent<LineRenderer>().SetPosition(1, transform.position);
+
+		if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1) {
+			_timeSittingAround += Time.deltaTime;
+		}
+
+		if (transform.position.y > 10 ||
+		transform.position.y < -10 ||
+		transform.position.x > 10 ||
+		transform.position.x < -10 ||
+		_timeSittingAround > 3) {
 			string currentSceneName = SceneManager.GetActiveScene().name;
 			SceneManager.LoadScene(currentSceneName);
 		}
 	}
 
 	private void OnMouseDown() {
-		GetComponent<SpriteRenderer>().color = Color.red;	
+		GetComponent<SpriteRenderer>().color = Color.red;
+		GetComponent<LineRenderer>().enabled = true;
 	}
 
 	private void OnMouseUp() {
@@ -26,6 +41,9 @@ public class Bird : MonoBehaviour
 		Vector2 directionToInitialPosition = _initialPosition - transform.position;
 		GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
 		GetComponent<Rigidbody2D>().gravityScale = 1;
+		_birdWasLaunched = true;
+
+		GetComponent<LineRenderer>().enabled = false;
 	}
 
 	private void OnMouseDrag() {
