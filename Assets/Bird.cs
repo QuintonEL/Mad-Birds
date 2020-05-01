7b50d072d67a0ef85d5b7ad31edc5e643083aaf3
QuintonEL <quintonlaborde@gmail.com>
2020-05-01 14:46:44 -0600

    physics now on bird for launching and reseting when out of bounds

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -1,19 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
-	private void OnMouseDown()
-	{
+	private Vector3 _initialPosition;
+	[SerializeField] private float _launchPower = 250;
+
+	private void Awake() {
+		_initialPosition = transform.position;
+	}
+
+	private void Update() {
+		if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -10) {
+			string currentSceneName = SceneManager.GetActiveScene().name;
+			SceneManager.LoadScene(currentSceneName);
+		}
+	}
+
+	private void OnMouseDown() {
 		GetComponent<SpriteRenderer>().color = Color.red;	
 	}
 
-	private void OnMouseUp()
-	{
+	private void OnMouseUp() {
 		GetComponent<SpriteRenderer>().color = Color.white;
+		Vector2 directionToInitialPosition = _initialPosition - transform.position;
+		GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+		GetComponent<Rigidbody2D>().gravityScale = 1;
 	}
 
-	private void OnMouseDrag()
-	{
+	private void OnMouseDrag() {
 		Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		transform.position = new Vector3(newPosition.x, newPosition.y);
 	}
